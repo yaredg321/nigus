354192fee155b21920e00ab69eb1fbac51d942f6
yaredg321 <yaredg321@gmail.com>
2022-07-18 17:50:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+
+  
+  const [data,setdata]=useState([         
+    {
+    "id": 22,
+    "quantity": 50,
+    "pricePerPiece": 25,
+    "purchasedOn": "2022-06-01T10:44:50.186Z"
+  },
+  {
+    "id": 23,
+    "quantity": 50,
+    "pricePerPiece": 25,
+    "purchasedOn": "2022-06-01T10:47:02.185Z"
+  },
+  {
+    "id": 24,
+    "quantity": 50,
+    "pricePerPiece": 25,
+    "purchasedOn": "2022-06-01T10:47:47.283Z"
+  },
+  {
+    "id": 25,
+    "quantity": 50,
+    "pricePerPiece": 25,
+    "purchasedOn": "2022-06-01T10:48:12.616Z"
+  },
+  
+]) 
+
+const postdata = (quantity,pricePerPiece) =>{
+  setdata([...data,{quantity},{pricePerPiece} ])
+}
+
+const datefilter = (value)=>{
+  setdata(data.filter(item=>(item.purchasedOn == value)
+  
+  ))
+}
+  
+ 
+
+return (
+  
+  
+    <div id='div'>
+     
+      
+      <form id='fo' onClick={datefilter} method='post'>
+      <label>date 1</label>
+     <input type="date"  placeholder='date1' onChange={(e)=>{datefilter(e.target.value)}}/>
+     <label>data 2</label>
+     <input type="date"  placeholder='date2' onChange={(e)=>{datefilter(e.target.value)}}/>
+     <input type="submit"  placeholder='filterd by date '/>
+      </form>
+     
+     <label>stattPosition</label>
+     <input type="text" placeholder=''/>
+     <label>maxResult</label>
+     <input type="text" placeholder=''/>
+
+
+     <form id='form' method='post' onSubmit={postdata}>
+    
+     <h2>add  data</h2>
+     <input type="text" name='quantity' placeholder='enter your qintity'/>
+     
+     
+     <input type="text" name='pricePerPiece' placeholder='enter your pricePerPiece'/>
+     <input type="submit"/>
+    
+     </form>
+
+     
+     {data.map(ma=>(
+         <div key={ma.id}>
+       
+         <h1>id :{ma.id}</h1>
+
+         <h3>{ma.purchasedOn}</h3>
+       </div>
+      
+
+      )
+      
+       
+      
+     )}
+     
+
     </div>
   );
 }
